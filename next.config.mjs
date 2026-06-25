e3e7ed9239c40fe00069f5495cecaf9c5d99f9d7
mansh <manshabhasin9@gmail.com>
2026-06-25 10:55:23 -0500

    Allow glade-zeta.vercel.app in server actions

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
   },
   experimental: {
     serverActions: {
-      allowedOrigins: ["localhost:3000"],
+      allowedOrigins: ["localhost:3000", "glade-zeta.vercel.app"],
     },
   },
 };
